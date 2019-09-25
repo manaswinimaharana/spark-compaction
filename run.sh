31aeb57e7d6a4f69915252075bff0c17dc10fbfc
Manaswini Maharana <mmaharana@mmaharana-MBP13.local>
2019-09-24 18:10:05 -0700

    added a wrapper

--- run.sh
+++ run.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 uid=`uuidgen`
 
+jarPath=${1}
+ip=${2}
+op=${3}
+ic=${4}
+is=${5}
+oc=${6}
+os=${7}
+pm=${8}
+
+
+
 
 spark2-submit \
   --class org.cloudera.com.spark_compaction.HdfsCompact \
   --master local[2] \
-  /tmp/spark-compaction-0.0.1-SNAPSHOT.jar --input-path /tmp/compression_input --output-path /tmp/compression_output6 --input-compression none --input-serialization text --output-compression none --output-serialization text --partition-mechanism repartition
+  ${jarPath} \
+  --input-path ${ip} \
+  --output-path ${op} \
+  --input-compression ${ic} \
+  --input-serialization ${is} \
+  --output-compression ${oc} \
+  --output-serialization ${os} \
+  --partition-mechanism ${pm}
