41ad5124f602298a2d36302f2c1aeb8e928f379f
Manaswini Maharana <mmaharana@mmaharana-MBP13.local>
2019-09-24 18:41:42 -0700

    added a wrapper

--- run.sh
+++ run.sh
@@ -10,8 +10,23 @@ oc=${6}
 os=${7}
 pm=${8}
 
+if ! `hdfs dfs -test -e ${ip}`;
+ then
+    echo "${ip} does not exists on HDFS"
+    exit 1
+fi
 
+if ! `hdfs dfs -test -d ${ip}`;
+ then
+    echo "${ip} is not a directory"
+    exit 1
+fi
 
+if ! `hdfs dfs -test -s ${op}`;
+ then
+    echo "${op} directory already exists"
+    exit 1
+fi
 
 spark2-submit \
   --class org.cloudera.com.spark_compaction.HdfsCompact \
